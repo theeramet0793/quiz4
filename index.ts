fb6291643caa45343f785c91952cc4e748c65599
theeramet0793 <59278790+theeramet0793@users.noreply.github.com>
2021-03-09 14:40:12 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,81 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { body, query, validationResult } from 'express-validator'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 3000
+const SECRET = "SIMPLE_SECRET"
+
+interface JWTPayload {
+  username: string;
+  password: string;
+}
+
+app.post('/login',
+  (req, res) => {
+
+    const { username, password } = req.body
+    // Use username and password to create token.
+
+    return res.status(200).json({
+      message: 'Login succesfully',
+    })
+  })
+
+app.post('/register',
+  (req, res) => {
+
+    const { username, password, firstname, lastname, balance } = req.body
+  })
+
+app.get('/balance',
+  (req, res) => {
+    const token = req.query.token as string
+    try {
+      const { username } = jwt.verify(token, SECRET) as JWTPayload
+  
+    }
+    catch (e) {
+      //response in case of invalid token
+    }
+  })
+
+app.post('/deposit',
+  body('amount').isInt({ min: 1 }),
+  (req, res) => {
+
+    //Is amount <= 0 ?
+    if (!validationResult(req).isEmpty())
+      return res.status(400).json({ message: "Invalid data" })
+  })
+
+app.post('/withdraw',
+  (req, res) => {
+  })
+
+app.delete('/reset', (req, res) => {
+
+  //code your database reset here
+  
+  return res.status(200).json({
+    message: 'Reset database successfully'
+  })
+})
+
+app.get('/me', (req, res) => {
+  
+})
+
+app.get('/demo', (req, res) => {
+  return res.status(200).json({
+    message: 'This message is returned from demo route.'
+  })
+})
+
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
